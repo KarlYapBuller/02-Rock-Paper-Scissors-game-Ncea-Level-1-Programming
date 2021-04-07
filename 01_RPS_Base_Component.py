796fc2d1d2992d32416a797ed1b591cbc5ad7c73
KarlYapBuller <78565635+KarlYapBuller@users.noreply.github.com>
2021-04-07 14:43:01 +1200

    Update 01_RPS_Base_Component.py

---
+++ 01_RPS_Base_Component.py
@@ -0,0 +1,101 @@
+import random
+
+#Functions
+
+#Round Mechanics Function
+
+#User Choice Rock,Paper,Scissors
+
+def choice_checker(question, valid_list, error_message):
+
+    valid = False
+    while not valid:
+
+        #Ask user for choice and pit choice in lowercase
+        response = input(question).lower()
+
+        #Iterates through list anf if response is an item
+        #In the list (or the first letter of an item),
+        #The full item name is returned
+
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+
+        #Output error if the item is not in the list
+        print(error_message)
+        print()
+
+def check_rounds():
+    while True:
+        response = input ("How many rounds: ")
+
+        round_error = "Please type either <enter> or an integer that is more than 0"
+#If ifinite mode is not chosen, check response is an integer more than 0
+        if response != "":
+            try:
+                response = int(response)
+
+#If response is too low, go back to the start of the loop and display an error message to help user
+                if response < 1:
+                    print(round_error)
+                    continue
+
+            except ValueError:
+                print(round_error)
+                print()
+                continue
+
+        return response
+
+#Main Routine
+
+#Lists for valid input for checking responses
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+rounds_played = 0
+
+rounds = check_rounds()
+
+end_game = "no"
+while end_game == "no":
+
+#Round heading
+
+    print()
+    if rounds == "":
+        heading = "Continuous Mode: Round {}".format(rounds_played + 1)
+
+
+    else:
+        heading = "Round {} of {}".format(rounds_played + 1, rounds)
+
+    print(heading)
+
+    choose_instruction = "Please choose rock (r), paper (p) or scissors (s) or 'xxx to quit the game: "
+
+    choose_error = "<Error> Please choose from rock (r), paper (p) or scissors (s)"
+
+#Ask user for choice and check it is valid
+    choose = choice_checker(choose_instruction, rps_list, choose_error)
+
+#Get Computer Choice
+    computer_choice = random.choice(rps_list[:-1])
+    print("Computer Choice: ", computer_choice)
+
+#Compare User and Computer Choice
+
+#End game if exit code is typed
+    if choose == "xxx":
+        break
+
+#rest of loop/game
+    print("You chose {}".format(choose))
+
+    rounds_played += 1
+
+#End game if the number of rounds has been played
+    if rounds_played == rounds:
+        break
+
+print("Thank you for playing")
